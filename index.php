81bf0db18c6c7e1cd8fdc2cbda50a50cf22ccd57
Jovan <klikovacbogdan@gmail.com>
2020-09-13 19:16:21 +0200

    Bootstrap - small change

--- index.php
+++ index.php
@@ -1,7 +1,43 @@
-<form class="form-horizontal" action="uploadCsv.php" method="post" name="uploadCsv" enctype="multipart/form-data">
-    <div>
-        <label>Choose CSV File</label>
-        <input type="file" name="file" accept=".csv">
-        <button type="submit" name="import">Import</button>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>BoolienIt</title>
+</head>
+<body>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+
+<div class="card" style="width: 38rem; margin: auto;">
+    <div class="card-header">
+        Test za junior programera Boolean IT
     </div>
-</form>
+    <div class="card-body">
+        <form class="form-group" action="uploadCsv.php" method="post" name="uploadCsv" enctype="multipart/form-data">
+            <div>
+                <h5 class="card-title">Choose CSV File</h5>
+                <input type="file" name="file" accept=".csv">
+                <button type="submit" name="import" class="btn btn-primary">Import</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
+
+
+
+
+
