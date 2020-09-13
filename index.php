7a287a7713558262a222e2f5466af0950951ea50
Jovan <klikovacbogdan@gmail.com>
2020-09-13 13:08:38 +0200

    Import CSV file

---
+++ index.php
@@ -0,0 +1,7 @@
+<form class="form-horizontal" action="uploadCsv.php" method="post" name="uploadCsv" enctype="multipart/form-data">
+    <div>
+        <label>Choose CSV File</label>
+        <input type="file" name="file" accept=".csv">
+        <button type="submit" name="import">Import</button>
+    </div>
+</form>
---
+++ uploadCsv.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once 'config/Db.php';
+
+
+
+//Instantiate DB
+$database = new Db();
+$db = $database->connect();
+
+if(isset($_POST["import"])){
+    $fileName = explode(".",$_FILES["file"]["name"]);
+
+    if($_FILES["file"]["tmp_name"]) {
+        if(end($fileName) == "csv") {
+            $file = fopen($_FILES["file"]["tmp_name"], "r");
+
+            while(($column = fgetcsv($file, 1000, ",")) != FALSE) {
+                if($row > 0) {
+                    $prep = $db->prepare("INSERT into products (model_number, category_name, departmant_name, manufacturer_name, upc, sku, regular_price, sale_price, description, url) values (:model_number, :category_name, :departmant_name, :manufacturer_name, :upc, :sku, :regular_price, :sale_price, :description, :url)");
+                    $prep->bindParam(':model_number', $column[0]);
+                    $prep->bindParam(':category_name', $column[1]);
+                    $prep->bindParam(':departmant_name', $column[2]);
+                    $prep->bindParam(':manufacturer_name', $column[3]);
+                    $prep->bindParam(':upc', $column[4]);
+                    $prep->bindParam(':sku', $column[5]);
+                    $prep->bindParam(':regular_price', $column[6]);
+                    $prep->bindParam(':sale_price', $column[7]);
+                    $prep->bindParam(':description', $column[8]);
+                    $prep->bindParam(':url', $column[9]);
+
+                    $prep->execute();
+                    
+                }
+                $row++;
+            }
+        } else {
+            echo "Please Select CSV File";
+        }
+    } else {
+        echo "Please Select File";
+    }
+}
+
+?>
